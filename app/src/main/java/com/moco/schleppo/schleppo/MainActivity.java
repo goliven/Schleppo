25647e71be1443437a8f3a3a9de2b1c828fb9a23
Franzissa <franzisssa@gmail.com>
2016-10-23 16:14:02 +0200

    1. Version Mail Activity als Table View & Profilansicht

--- app/src/main/java/com/moco/schleppo/schleppo/MainActivity.java
+++ app/src/main/java/com/moco/schleppo/schleppo/MainActivity.java
@@ -2,12 +2,46 @@ package com.moco.schleppo.schleppo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    Button btnmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.MyButton);
+        btnmail = (Button) findViewById(R.id.btnmails);
+
+        // Capture button clicks
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        profil.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btnmail.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        MailListActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
+
 }
